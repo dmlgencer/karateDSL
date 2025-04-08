ae4f7a9df0512a8d819882cc317a96f593cf385e
damlagencer <d.gencer@rail-flow.com>
2025-04-08 14:06:30 +0300

    raporlama

--- src/test/java/runner/KarateRunner.java
+++ src/test/java/runner/KarateRunner.java
@@ -6,7 +6,7 @@ import com.intuit.karate.junit5.Karate;
 public class KarateRunner {
     @Karate.Test
     Karate runTest(){
-        return Karate.run("src/test/java/features/get.feature").tags("@getTest");
+        return Karate.run("src/test/java/features/postWithParameters.feature").tags("@postWithParameters");
     }
 }
 
